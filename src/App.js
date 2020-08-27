96e6bbe51b7433fa9c5a80455d43db973dd433c9
Ryan L <codelinde@gmail.com>
2020-08-26 20:24:34 -0700

    Add basic source files
    Starting from the create-react-app, I have created my own drum machine app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import drumKeys from './drumKeys.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      kitDisplay: "press a key",
+      kitCode: 0
+    };
+    this.handleEntry = this.handleEntry.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+
+  handleEntry(code) {
+    this.setState({
+      kitDisplay: drumKeys[code].descript,
+      kitCode: drumKeys[code]
+    });
+
+  }
+
+  handleKeyPress(event) {
+    const pressCodes = [81, 87, 69, 65, 83, 68, 90, 88, 67];
+    if (pressCodes.includes(event.keyCode)) {
+      this.handleEntry(event.keyCode);
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+      <div className="top">
+        <h1>Drum Machine</h1>
+      </div>
+      <div className="grid">
+        <div className="pad">Q</div>
+        <div className="pad">W</div>
+        <div className="pad">E</div>
+        <div className="pad">A</div>
+        <div className="pad">S</div>
+        <div className="pad">D</div>
+        <div className="pad">Z</div>
+        <div className="pad">X</div>
+        <div className="pad">C</div>
+      </div>
+      <div className="bottom">
+        {this.state.kitDisplay}
+      </div>
     </div>
-  );
+    );
+  }
 }
+
 
 export default App;
---
+++ src/drumKeys.js
@@ -0,0 +1,67 @@
+const drumKeys = {
+    81: {
+        letter: "Q",
+        descript: "kick",
+        sound: "/sounds/kick.wav",
+        keyDownUp: 81,
+        keyPress: 113
+    },
+    87: {
+        letter: "W",
+        descript: "snare",
+        sound: "/sounds/snare.wav",
+        keyDownUp: 87,
+        keyPress: 119
+    },
+    69: {
+        letter: "E",
+        descript: "clap",
+        sound: "/sounds/clap.wav",
+        keyDownUp: 69,
+        keyPress: 101
+    },
+    65: {
+        letter: "A",
+        descript: "hi-hat open",
+        sound: "/sounds/hi-hat-open.wav",
+        keyDownUp: 65,
+        keyPress: 97
+    },
+    83: {
+        letter: "S",
+        descript: "hi-hat closed",
+        sound: "/sounds/hi-hat-closed.wav",
+        keyDownUp: 83,
+        keyPress: 115
+    },
+    68: {
+        letter: "D",
+        descript: "tom 1",
+        sound: "/sounds/tom-1.wav",
+        keyDownUp: 68,
+        keyPress: 100
+    },
+    90: {
+        letter: "Z",
+        descript: "tom 2",
+        sound: "/sounds/tom-2.wav",
+        keyDownUp: 90,
+        keyPress: 122
+    },
+    88: {
+        letter: "X",
+        descript: "tom 3",
+        sound: "/sounds/tom-3.wav",
+        keyDownUp: 88,
+        keyPress: 120
+    },
+    67: {
+        letter: "C",
+        descript: "shaker",
+        sound: "/sounds/shaker.wav",
+        keyDownUp: 67,
+        keyPress: 99
+    }
+};
+
+export default drumKeys;
